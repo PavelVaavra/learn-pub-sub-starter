da72a53cb6f1719f6c50b41b44a11272a41e38e2
PavelVaavra <pavelvavra.vm@gmail.com>
2026-04-18 19:31:07 +0200

    Exchanges and Queues

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -13,12 +16,21 @@ func main() {
 	url := "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(url)
 	if err != nil {
-		fmt.Println("Error connecting to RabbitMQ:", err)
+		log.Fatalf("Error connecting to RabbitMQ: %v", err)
 		return
 	}
 	defer conn.Close()
-
 	fmt.Println("Connected to RabbitMQ")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Error creating channel: %v", err)
+		return
+	}
+
+	pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
